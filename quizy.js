5b694a29aa820d40aed86491de5d55372bb70705
Kanta Ono <kanta20021476@gmail.com>
2021-09-26 00:37:45 +0900

    完成

--- quizy.js
+++ quizy.js
@@ -1,28 +1,6 @@
 console.log('kakuninnsimasita')
 
-// const kanta = document.getElementById('question1-1')
 
-
-// const kanta1 = kanta.addEventListener('click',()=>{
-//     kanta.style.background = 'blue';
-// });
-
-// let content={
-// '<h2><p class="question_question">' :+count+',この地名なんと読む？'+'</p></h2>'+
-//     '<img src="https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png">'+
-//     '<ul>'+
-//         '<div class="li" id=question'+count+'-true>たかなわ</div>'+
-//         '<div class="li" id=question'+count+'-false1>たかわ</div>'+
-//         '<div class="li" id=question'+count+'-false2>こうわ</div>'+
-//     '</ul>'+
-//     '<section class="answer_box" id="answer-box-'+count+'">'+
-//         '<span class="q_true" id="q'+count+'-true">正解！</span>'+
-//         '<span class="q_false" id="q'+count+'-false">不正解！</span>'+
-//         '<div class="q_answer" id="q'+count+'-answer">'+
-//             '正解は「たかなわ」です！'
-//         +'</div>'+
-//     '</section>'
-// }
 let container = document.getElementById('container');
 let quizSet = [
  ['たかなわ','たかわ','こうわ'],
@@ -48,138 +26,56 @@ let picture = [
     ['https://d1khcm40x1j0f.cloudfront.net/words/8cad76c39c43e2b651041c6d812ea26e.png'],
     ['https://d1khcm40x1j0f.cloudfront.net/words/34508ddb0789ee73471b9f17977e7c9c.png'],
 ];
+
 let content = "";
 for(let count=0;count < 10 ; count++){
+    var shuffleArray = [0,1,2];
+    //シャッフルアレイは３個あるから2回for文が行われている
+    
+    for (let i = shuffleArray.length - 1; i > 0; i--) {
+        //０〜2.９までの乱数を出し、floorで小数点以下切り捨て
+      const j = Math.floor(Math.random() * (i + 1));
+      //iとjを入れ替えている、これをfor文分やっている（2回）
+      [shuffleArray[j], shuffleArray[i]] = [shuffleArray[i], shuffleArray[j]];
+    }
+    console.log(shuffleArray);
     content +=
         '<h2><p class="question_question">' +[count+1]+',この地名なんと読む？'+'</p></h2>'+
             '<img src='+picture[count]+'>'+
             '<div>'+
-                '<div class="li" id="question'+count+'-true" >'+quizSet[count][0]+'</div>'+
-                '<div class="li" id="question'+count+'-false1">'+quizSet[count][1]+'</div>'+
-                '<div class="li" id="question'+count+'-false2">'+quizSet[count][2]+'</div>'+
+                '<div class="li" id="question'+count+'-'+shuffleArray[0]+'" onclick = "clickfunction('+count+','+shuffleArray[0]+')">'+quizSet[count][shuffleArray[0]]+'</div>'+
+                '<div class="li" id="question'+count+'-'+shuffleArray[1]+'" onclick = "clickfunction('+count+','+shuffleArray[1]+')">'+quizSet[count][shuffleArray[1]]+'</div>'+
+                '<div class="li" id="question'+count+'-'+shuffleArray[2]+'" onclick = "clickfunction('+count+','+shuffleArray[2]+')">'+quizSet[count][shuffleArray[2]]+'</div>'+
             '</div>'+
-            '<section class="answer_box" id="answer-box">'+
-                '<span class="q_true" id="q-true">正解！</span>'+
-                '<span class="q_false" id="q-false">不正解！</span>'+
+            '<section class="answer_box" id="answer-box-'+count+'">'+
+                '<span class="q_true" id="q-'+count+'-true">正解！</span>'+
+                '<span class="q_false" id="q-'+count+'-false">不正解！</span>'+
                 '<div class="q_answer" id="q'+count+'-answer">'+
                     '正解は「'+quizSet[count][0]+'」です！'
                 +'</div>'+
             '</section>'
 container.innerHTML = content;
+}
 
-// if () {
-        
-// } else {
-    
-// }
+console.log(shuffleArray);
+   
 
 
-
-
-
-
-// function shuffle(arr) {
-//     for (let i = arr.length - 1; i > 0; i--) {
-//       const j = Math.floor(Math.random() * (i + 1));
-//       [arr[j], arr[i]] = [arr[i], arr[j]];
-//     }
-//     return arr;
-//   }
-
-//   const shuffledChoices = shuffle([...quizSet[currentNum].c]);
-//   shuffledChoices.forEach(choice => {
-//     const li = document.createElement('li');
-//     li.textContent = choice;
-//     choices.appendChild(li);
-//   });
-
-
-
-
-
-// let serectTrue = document.getElementById('question'+count+'-true');
-// let serectFalse1 = document.getElementById('question'+count+'-false1');
-// let serectFalse2 = document.getElementById('question'+count+'-false2');
-// let result = document.getElementById('answer-box');
-// let correctAnswer = document.getElementById('q-true');
-// let wrongAnswer = document.getElementById('q-false');
-
-
-//     serectTrue.addEventListener("click", ok);
-//     serectFalse1.addEventListener("click", error1);
-//     serectFalse2.addEventListener("click", error2);
-
-
-//     function ok() {
-//         serectTrue.classList.add("");
-//         serectFalse1.classList.remove("false");
-//         serectFalse2.classList.remove("false");
-
-//         correctAnswer.classList.remove("disAppear");
-//         incorrect.classList.add("disAppear");
-//     }
-
-//     function error1() {
-//         serectTrue.classList.remove("right");
-//         serectFalse1.classList.add("false");
-//         serectFalse2.classList.remove("false");
-  
-//         incorrect.classList.remove('disAppear');
-//         correctAnswer.classList.add('disAppear')
-//     }
-
-//     function error2() {
-//         serectTrue.classList.remove("right");
-//         serectFalse1.classList.remove("false");
-//         serectFalse2.classList.add("false");
-  
-//         incorrect.classList.remove('disAppear');
-//         correctAnswer.classList.add('disAppear');
-//     }
-    
-
-
-        
-    document.getElementById('question'+count+'-true').addEventListener('click',()=>{
-        console.log('あ');
-        document.getElementById('question'+count+'-true').style.background = '#287dff';
-        document.getElementById('question'+count+'-true').style.color = 'white';
-        document.getElementById('answer-box-'+count).style.display="block";
-        document.getElementById('q'+count+'-true').style.display="block";
-        // document.getElementById('q'+count+'-false').style.display="none";
-        document.getElementById('question'+count+'-true').color = 'white';
-        document.getElementById('question'+count+'-true').classList.add('after');
-        document.getElementById('question'+count+'-false1').classList.add('after');
-        document.getElementById('question'+count+'-false2').classList.add('after');
-    });
-    document.getElementById('question'+count+'-false1').addEventListener('click',()=>{
-        document.getElementById('question'+count+'-true').style.background = '#287dff';
-        document.getElementById('question'+count+'-false1').style.background = 'red';
-        document.getElementById('question'+count+'-true').style.color = 'white';
-        document.getElementById('question'+count+'-false1').style.color = 'white';
-        document.getElementById('answer-box-'+count).style.display="block";
-        document.getElementById('q'+count+'-false').style.display="block";
-        // document.getElementById('q'+count+'-true').style.display="none";
-        document.getElementById('question'+count+'-true').classList.add('after');
-        document.getElementById('question'+count+'-false1').classList.add('after');
-        document.getElementById('question'+count+'-false2').classList.add('after');
-    });
-    document.getElementById('question'+count+'-false2').addEventListener('click',()=>{
-        document.getElementById('question'+count+'-true').style.background = '#287dff';
-        document.getElementById('question'+count+'-false2').style.background = 'red';
-        document.getElementById('question'+count+'-true').style.color = 'white';
-        document.getElementById('question'+count+'-false2').style.color = 'white';
-        document.getElementById('answer-box-'+count).style.display="block";
-        document.getElementById('q'+count+'-false').style.display="block";
-        // document.getElementById('q'+count+'-true').style.display="none";
-        document.getElementById('question'+count+'-true').classList.add('after');
-        document.getElementById('question'+count+'-false1').classList.add('after');
-        document.getElementById('question'+count+'-false2').classList.add('after');
-    });
-      
-
-
-
-
-
+//１問目の１選択肢目を選んだ時　clickfunction(0,0)
+function clickfunction (questionNo,click) {
+        document.getElementById('question'+questionNo+'-'+click).style.background = 'red';
+        document.getElementById('question'+questionNo+'-'+0).style.background = 'blue';
+        document.getElementById('question'+questionNo+'-'+click).style.color = 'white';
+        document.getElementById('question'+questionNo+'-'+0).style.color = 'white';
+        document.getElementById('answer-box-'+questionNo).style.display="block";
+        document.getElementById('q-'+questionNo+'-true').style.display="none";
+        document.getElementById('q-'+questionNo+'-false').style.display="none";
+        document.getElementById('question'+questionNo+'-0').classList.add('after');
+        document.getElementById('question'+questionNo+'-1').classList.add('after');
+        document.getElementById('question'+questionNo+'-2').classList.add('after');
+        if(click===0){
+            document.getElementById('q-'+questionNo+'-true').style.display="block";
+        }else{
+            document.getElementById('q-'+questionNo+'-false').style.display="block";
+        }
 }
