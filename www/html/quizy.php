1cd5ea436471acfb4da2c9d54341dc597daf6bcf
Kanta Ono <kanta20021476@gmail.com>
2022-01-19 02:25:16 +0900

    母は

--- www/html/quizy.php
+++ www/html/quizy.php
@@ -15,6 +15,7 @@ require("./db_connect.php");
 
 <body>
     <h1><p class="quiz-title">ガチで東京の人しか解けない！ #東京の難読地名クイズ</p></h1>
+    <!-- 無駄な一文 -->
     
     <div id = "container"></div>
     <script src="quizy.js"></script>
