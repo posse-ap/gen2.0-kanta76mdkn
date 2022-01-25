f1993befc422ee9dd24a1ab3f6ca84a02bd79cef
silent.bayline <silent.bayline@gmail.com>
2022-01-25 21:23:32 +0900

    test

--- www/html/quizy.php
+++ www/html/quizy.php
@@ -1,1 +1,24 @@
-小谷帝国作ります！
+<?php
+require("./db_connect.php");
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">X-UA-Compatible
+    <title>testtesttest</title>
+    <link rel="stylesheet" href="quizy.css">
+    <link rel="stylesheet" href="riset.css">
+</head>
+
+<body>
+    <h1><p class="quiz-title">ガチで東京の人しか解けない！ #東京の難読地名クイズ</p></h1>
+    <!-- 無駄な一文 -->
+    
+    <div id = "container"></div>
+    <script src="quizy.js"></script>
+</body>
+
+</html>
